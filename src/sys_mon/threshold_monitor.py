61f9a478e7222fb0ea95a292cc6064f7268b7ccd
bossyshrimp68 <romypine007@gmail.com>
2026-03-28 16:50:39 +0300

    simple notification

--- src/sys_mon/threshold_monitor.py
+++ src/sys_mon/threshold_monitor.py
@@ -1,10 +1,12 @@
-import asyncio
-import signal
 import threading
+import time
+from plyer import notification
 
 from sys_mon import collector
 
-from desktop_notifier import DesktopNotifier, Urgency, Button, ReplyField, DEFAULT_SOUND
+CPU_INTERVAL = 25
+RAM_INTERVAL = 25
+NOTIFICATION_TIMEOUT = 5
 
 cpu_threshold = 0
 ram_threshold = 0
@@ -28,44 +30,25 @@ def ram_breached():
     return ram_percentage > ram_threshold
 
 
-# def notify():
-
-
-async def main() -> None:
-    notifier = DesktopNotifier(app_name="System monitor")
-
-    await notifier.send(
+def notify(message):
+    notification.notify(
         title="Threshold breach",
-        message="Et tu, Brute?",
-        urgency=Urgency.Critical,
-        buttons=[
-            Button(
-                title="Mark as read",
-                on_pressed=lambda: print("Marked as read"),
-            )
-        ],
-        reply_field=ReplyField(
-            on_replied=lambda text: print("Brutus replied:", text),
-        ),
-        on_dispatched=lambda: print("Notification showing"),
-        on_clicked=lambda: print("Notification clicked"),
-        on_dismissed=lambda: print("Notification dismissed"),
-        sound=DEFAULT_SOUND,
+        message=message,
+        timeout=NOTIFICATION_TIMEOUT
     )
-
-    # # Run the event loop forever to respond to user interactions with the notification.
-    stop_event = asyncio.Event()
-    # loop = asyncio.get_running_loop()
-    #
-    # loop.add_signal_handler(signal.SIGINT, stop_event.set)
-    # loop.add_signal_handler(signal.SIGTERM, stop_event.set)
-    #
-    await stop_event.wait()
 
 
 def thread_func():
+    global cpu_threshold, ram_threshold
+
+    cpu_timer = -CPU_INTERVAL
+    ram_timer = -RAM_INTERVAL
+
     while True:
-        if cpu_breached():
-            asyncio.run(main())
-        if ram_breached():
-            asyncio.run(main())
+        if cpu_breached() and (time.time() - cpu_timer >= CPU_INTERVAL):
+            notify(f"cpu threshold {cpu_threshold}% breached")
+            cpu_timer = time.time()
+
+        if ram_breached() and (time.time() - ram_timer >= RAM_INTERVAL):
+            notify(f"ram threshold {ram_threshold}% breached")
+            ram_timer = time.time()
